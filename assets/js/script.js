6f44b7e7aea5d2e900b0041ced34bcdb8ca4ad03
CodingWithAbdou <coder.abdou@gmail.com>
2023-10-10 14:59:27 +0100

    - Upload The Design With Tow Lang Arbic && English

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// PRELOAD
+
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("load",  () => {
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded");
+});
+
+
+// GET YEAR 
+
+let currentDate = new Date().getFullYear();
+
+const date = document.querySelector("footer .date");
+
+date.innerText = currentDate;
+
